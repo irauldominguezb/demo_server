5f5c591a669f89b1721ef48e59996172a83187d8
Raúl Domínguez <20213tn103@utez.edu.mx>
2024-02-06 14:24:49 -0600

    [ADD] creacion de funcion savePet()

--- app.js
+++ app.js
@@ -21,4 +21,54 @@ const createClient = () => {
         });
 }
 
-setInterval(createClient,5000);
+const petNames = ["Max", "Luna", "Bella", "Charlie", "Lucy", "Cooper", "Oliver", "Mia", "Rocky", "Daisy", "Coco", "Bailey", "Sophie", "Leo", "Milo", "Zoe", "Chloe", "Toby", "Lola", "Oscar"]
+const breed = ["Labrador", "Bulldog", "Poodle", "Beagle", "Rottweiler", "Husky", "Boxer", "Dalmatian", "Doberman", "Chihuahua", "Pug", "Shih Tzu", "Golden Retriever", "Schnauzer", "Yorkshire Terrier", "Pomeranian", "Maltese", "Bichon Frise", "Cocker Spaniel", "Papillon"]
+const catBreed = ["Siamese", "Persian", "Main"]
+const type = ["perro", "gato"]
+
+const getClients = () => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT id FROM clients ORDER BY RAND() LIMIT 1';
+        db.query(sql, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                const {id} = result[0] 
+                resolve(id);
+            }
+        });
+    });
+};
+
+
+getRandNumber = (max) => {
+    return Math.floor(Math.random() * max)
+}
+
+
+const savePet = async () => {
+    try {
+        const idClient = await getClients();
+        const petName = petNames[getRandNumber(petNames.length)];
+        const petType = type[getRandNumber(type.length)];
+        let petBreed = "";
+        switch (petType) {
+            case "perro":
+                petBreed = breed[getRandNumber(breed.length)];
+                break;
+            case "gato":
+                petBreed = catBreed[getRandNumber(catBreed.length)];
+                break;
+            default:
+                break;
+        }
+        const sql = `INSERT INTO pets (name, type, breed, owner_id) VALUES ('${petName}','${petType}', '${petBreed}', ${idClient})`;
+        db.query(sql, (err, result)=> {
+            if(err) throw err;
+            console.log('Mascota creada!', result);
+        })
+    } catch (error) {
+        console.error('Error al obtener el cliente:', error);
+    }
+};
+setInterval(savePet,5000);
